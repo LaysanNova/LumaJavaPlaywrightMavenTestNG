7a697e72768afae37d1331ab9f56e4f5a7c33883
laysan <laysansh@gmail.com>
2023-12-23 16:30:12 -0700

    logging error

--- src/test/java/tests/HomeTest.java
+++ src/test/java/tests/HomeTest.java
@@ -7,15 +7,14 @@ public class HomeTest extends BaseTest {
 
     @Test
     public void testLoginNavigation() {
+
+        System.out.println("Passed");
+        System.out.println("Passed");
         System.out.println("Passed");
     }
 
     @Test
     public void testTest() {
         System.out.println("Passed");
-
-
-
-
     }
 }
